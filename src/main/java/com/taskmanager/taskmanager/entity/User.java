e38e38e8d8f4aab550ac554cce1fee44f3da8b44
gm-sourav <souravmahbub13@gmail.com>
2026-08-17 22:52:32 +0600

    4th

--- src/main/java/com/taskmanager/taskmanager/entity/User.java
+++ src/main/java/com/taskmanager/taskmanager/entity/User.java
@@ -1,4 +1,36 @@
 package com.taskmanager.taskmanager.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name="users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Task> tasks;
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
