7621dcea02aa2823a356f813688104be6cf3a1ef
gm-sourav <souravmahbub13@gmail.com>
2026-08-17 23:44:32 +0600

    6th

--- src/main/java/com/taskmanager/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/taskmanager/taskmanager/repository/TaskRepository.java
@@ -1,4 +1,4 @@
 package com.taskmanager.taskmanager.repository;
 
-public class TaskRepository {
+public interface TaskRepository {
 }
--- src/main/java/com/taskmanager/taskmanager/repository/UserRepository.java
+++ src/main/java/com/taskmanager/taskmanager/repository/UserRepository.java
@@ -1,4 +1,4 @@
 package com.taskmanager.taskmanager.repository;
 
-public class UserRepository {
+public interface UserRepository {
 }
