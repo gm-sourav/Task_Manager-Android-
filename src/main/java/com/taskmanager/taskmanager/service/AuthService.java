76ca169367a3f6439b33e44bda3ef30a866303bc
gm-sourav <souravmahbub13@gmail.com>
2026-08-19 09:24:21 +0600

    6th

--- src/main/java/com/taskmanager/taskmanager/service/AuthService.java
+++ src/main/java/com/taskmanager/taskmanager/service/AuthService.java
@@ -1,4 +1,42 @@
 package com.taskmanager.taskmanager.service;
 
+import com.taskmanager.taskmanager.dto.LoginRequest;
+import com.taskmanager.taskmanager.dto.RegisterRequest;
+import com.taskmanager.taskmanager.entity.User;
+import com.taskmanager.taskmanager.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User register(RegisterRequest request){
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new RuntimeException("Email already registered");
+        }
+        User user = new User();
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+
+        return userRepository.save(user);
+    }
+
+
+
+    public User login(LoginRequest request){
+        User user = userRepository.findByEmail(request.getEmail()).orElseThrow(() -> new RuntimeException("Invalid email or password"));
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new RuntimeException("Invalid email or password");
+        }
+        return user;
+    }
 }
