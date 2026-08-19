5544046ed22196ddc711687a45e994755b0506f3
gm-sourav <souravmahbub13@gmail.com>
2026-08-19 10:34:42 +0600

    6th

--- src/main/java/com/taskmanager/taskmanager/controller/AuthController.java
+++ src/main/java/com/taskmanager/taskmanager/controller/AuthController.java
@@ -1,4 +1,48 @@
 package com.taskmanager.taskmanager.controller;
 
+import com.taskmanager.taskmanager.dto.AuthResponse;
+import com.taskmanager.taskmanager.dto.LoginRequest;
+import com.taskmanager.taskmanager.dto.RegisterRequest;
+import com.taskmanager.taskmanager.entity.User;
+import com.taskmanager.taskmanager.security.JwtUtil;
+import com.taskmanager.taskmanager.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
+        try {
+            User user = authService.register(request);
+            String token = jwtUtil.generateToken(user.getEmail());
+            return ResponseEntity.ok(new AuthResponse(token,"Registration successful"));
+
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+        try {
+            User user = authService.login(request);
+            String token = jwtUtil.generateToken(user.getEmail());
+            return ResponseEntity.ok(new AuthResponse(token,"Login successful"));
+
+
+        }  catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 }
