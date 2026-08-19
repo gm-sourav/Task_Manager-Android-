ccbda63bfe4e425644974fee89c3be9296ef3934
gm-sourav <souravmahbub13@gmail.com>
2026-08-19 09:05:15 +0600

    6th

--- src/main/java/com/taskmanager/taskmanager/dto/AuthResponse.java
+++ src/main/java/com/taskmanager/taskmanager/dto/AuthResponse.java
@@ -1,8 +1,12 @@
 package com.taskmanager.taskmanager.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class AuthResponse {
+    private String token;
+    private String message;
 
 }
